7412be96871e5e5cf18ccdf042f04dcf632065a1
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-12 18:30:36 +0330

    style: format codes, remove spaces, ...

--- lib/src/test/test.dart
+++ lib/src/test/test.dart
@@ -3,42 +3,40 @@ part of clean_test;
 typedef TestFunction = dynamic Function();
 typedef VoidCallback = void Function();
 
-class TestContainer{
-
+class TestContainer {
   final Map<String, Test> _descToTest = <String, Test>{};
 
-  void operator []=(String desc, Test test){
+  void operator []=(String desc, Test test) {
     _descToTest.putIfAbsent(desc, () => test);
   }
 
-  VoidCallback? operator [](String desc){
+  VoidCallback? operator [](String desc) {
     Test? t = _descToTest[desc];
 
-    if(t == null)
+    if (t == null) 
       return null;
-    
+
     return () {
       TestFunction func = t.body;
       TestConfig? config = t.config;
 
-        test.test(
-          desc,
-          func,
-          testOn: config?.testOn,
-          skip: config?.skip,
-          onPlatform: config?.onPlatform,
-          retry: config?.retry,
-        );
+      test.test(
+        desc,
+        func,
+        testOn: config?.testOn,
+        skip: config?.skip,
+        onPlatform: config?.onPlatform,
+        retry: config?.retry,
+      );
     };
   }
 
-  void forEach(void Function(String desc) action){
-    _descToTest.forEach((key, value)=> action(key));
+  void forEach(void Function(String desc) action) {
+    _descToTest.forEach((key, value) => action(key));
   }
 }
 
-
-class Test{
+class Test {
   final TestFunction body;
   final TestConfig? config;
 
