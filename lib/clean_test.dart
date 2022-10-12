736dc91dfa874584898e0f82bc49e8d3913cff20
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-12 16:38:21 +0330

    clean project

--- lib/clean_test.dart
+++ lib/clean_test.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
 library clean_test;
 
-export 'src/clean_test_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
