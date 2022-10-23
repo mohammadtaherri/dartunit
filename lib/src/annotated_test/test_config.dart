578d8e4b5d9b6d6733d48b56ddc8205815d40343
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-24 02:14:10 +0330

    feat: create TestConfig

---
+++ lib/src/annotated_test/test_config.dart
@@ -0,0 +1,25 @@
+
+class TestConfig{
+  const TestConfig({
+    this.description,
+    this.skip = false,
+    this.testOn,
+    this.onPlatform,
+    this.retry,
+  });
+
+  final String? description;
+  final bool skip;
+  final String? testOn;
+  final Map<String, dynamic>? onPlatform;
+  final int? retry;
+
+  @override
+  String toString() => '''TestConfig (
+      description: $description, 
+      skip: $skip, 
+      testOn: $testOn, 
+      onPlatform: $onPlatform, 
+      retry: $retry
+    )''';
+}
