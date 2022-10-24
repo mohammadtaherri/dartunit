6b3c7f6e3d2736dfaeb5f9984e37101f49bb3208
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-24 23:44:46 +0330

    refactor: rename TestCaseMirror to TestSuiteFactoryForClass and move it to test_suite_factory.dart

--- lib/src/annotated_test/test_suite_factory.dart
+++ lib/src/annotated_test/test_suite_factory.dart
@@ -1,7 +1,6 @@
 
 import 'dart:mirrors';
 
-import 'test_case_mirror.dart';
 import 'test_case_object.dart';
 import 'extensions.dart';
 import 'test_config.dart';
@@ -24,7 +23,7 @@ class LibraryTestSuiteFactory implements TestSuiteFactory {
 
     for (final root in libMirror.rootTestCases)
       suites.add(
-        TestCaseMirror.create(
+        TestSuiteFactoryForClass.create(
           root,
           libMirror.subTestCases,
         ).createSuite(),
@@ -36,5 +35,115 @@ class LibraryTestSuiteFactory implements TestSuiteFactory {
     );
   }
 }
+
+
+class TestSuiteFactoryForClass implements TestSuiteFactory{
+
+  factory TestSuiteFactoryForClass.create(
+    ClassMirror rooTestCase,
+    List<ClassMirror> allSubTestCases,
+  ) {
+    TestSuiteFactoryForClass testCase = TestSuiteFactoryForClass._(rooTestCase);
+
+    for (final sub in allSubTestCases.of(rooTestCase))
+      testCase.addChild(TestSuiteFactoryForClass.create(sub, allSubTestCases));
+
+    return testCase;
+  }
+
+  TestSuiteFactoryForClass._(ClassMirror selfMirror)
+      : _selfMirror = selfMirror,
+        _children = List.empty(growable: true),
+        _setUpMirror = selfMirror.setUp,
+        _tearDownMirror = selfMirror.tearDown,
+        _setUpAllMirror = selfMirror.setUpAll,
+        _tearDownAllMirror = selfMirror.tearDownAll;
+
+  final ClassMirror _selfMirror;
+  final MethodMirror? _setUpMirror;
+  final MethodMirror? _tearDownMirror;
+  final MethodMirror? _setUpAllMirror;
+  final MethodMirror? _tearDownAllMirror;
+  final List<TestSuiteFactoryForClass> _children;
+  TestSuiteFactoryForClass? _parent;
+  
+
+  void addChild(TestSuiteFactoryForClass child){
+    child._parent = this;
+    _children.add(child);
+  }
+
+  @override
+  TestSuiteObject createSuite() {
+
+    Future<void> onSetUpAll() async{
+      if(_setUpAllMirror != null)
+        await _selfMirror.delegate(Invocation.method(_setUpAllMirror!.simpleName, []));
+    }
+
+    Future<void> onTearDownAll() async{
+      if(_tearDownAllMirror != null)
+        await _selfMirror.delegate(Invocation.method(_tearDownAllMirror!.simpleName, []));
+    }
+    
+    final List<TestCaseObjectBase> objects = List.empty(growable: true);
+
+    for(final testMirror in _selfMirror.tests)
+      objects.add(_createTestCaseObject(testMirror));
+
+    for(final child in _children)
+      objects.add(child.createSuite());
+
+    return TestSuiteObject(
+      testCaseObjects: objects,
+      onSetUpAll: onSetUpAll,
+      onTearDownAll: onTearDownAll,
+      config: _selfMirror.extractTestConfigIfPossible()!,
+    );
+  }
+
+  TestCaseObjectBase _createTestCaseObject(MethodMirror testMirror){
+    late final InstanceMirror selfInstance;
+    
+    Future<void> onSetUp() async{
+      selfInstance = _selfMirror.newInstance(Symbol.empty, []);
+
+      await _invokeSetUp(selfInstance);
+    }
+
+    Future<void> onTest() async{
+      await selfInstance.delegate(Invocation.method(testMirror.simpleName, []));
+    }
+
+    Future<void> onTearDown() async{
+      await _invokeTearDown(selfInstance);
+    }
+ 
+    return TestCaseObject(
+      onSetUp: onSetUp,  
+      onTest: onTest,
+      onTearDown: onTearDown,
+      config: testMirror.extractTestConfigIfPossible()!,
+    );
+  }
+
+  Future<void> _invokeSetUp(InstanceMirror instanceMirror) async{
+
+    await _parent?._invokeSetUp(instanceMirror);
+
+    if(_setUpMirror != null)
+      await instanceMirror.delegate(Invocation.method(_setUpMirror!.simpleName, []));
+  }
+
+  Future<void> _invokeTearDown(InstanceMirror instanceMirror) async{
+
+    if(_tearDownMirror != null)
+      await instanceMirror.delegate(Invocation.method(_tearDownMirror!.simpleName, []));
+  
+    await _parent?._invokeTearDown(instanceMirror);
+  } 
+}
+
+
 
 
