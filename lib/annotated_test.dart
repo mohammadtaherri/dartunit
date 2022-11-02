2bb311177268698bb08da5e613e36c0325356da1
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-02 19:55:27 +0330

    feat: add verify method to test package

--- lib/annotated_test.dart
+++ lib/annotated_test.dart
@@ -1,6 +1,12 @@
 library annotated_test;
 
+import 'package:test/test.dart';
+
 export 'package:test/test.dart';
 export './src/annotated_test/annotations.dart';
 export './src/annotated_test/runner.dart';
 
+
+void verify(bool actual){
+  expect(actual, isTrue);
+}
