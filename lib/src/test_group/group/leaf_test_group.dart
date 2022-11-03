4dd047656926c9ff2e20e012c97d24ac7c967c7d
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-03 17:15:49 +0330

    remove test_group

--- lib/src/test_group/group/leaf_test_group.dart
+++
@@ -1,8 +0,0 @@
-part of test_group;
-
-abstract class LeafTestGroup extends TestGroup {
-  LeafTestGroup({
-    super.groupDescription,
-    super.groupConfig,
-  });
-}
--- lib/src/test_group/runner.dart
+++
@@ -1,5 +0,0 @@
-part of test_group;
-
-void runTest(TestGroup root){
-  root.call();
-}
