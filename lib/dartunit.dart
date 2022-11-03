23a79c31e5788b1b19ea6652f601b275fc4ba971
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-03 17:28:11 +0330

    refactor: remove verify function

--- lib/dartunit.dart
+++ lib/dartunit.dart
@@ -1,12 +1,6 @@
 library dartunit;
 
-import 'package:test/test.dart';
-
 export 'package:test/test.dart';
 export 'src/annotations.dart';
 export 'src/runner.dart';
 
-
-void verify(bool actual){
-  expect(actual, isTrue);
-}
